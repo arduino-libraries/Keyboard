dfe39b47723b64617a36c2d97382fffd50215a11
Ryan Turner <zdbiohazard2@gmail.com>
2016-06-08 00:03:12 -0700

    Clean up the keyboard report descriptor.
    Reorder some items, remove a redundant item, make spacing consistent.

--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -28,36 +28,34 @@
 //	Keyboard
 
 static const uint8_t _hidReportDescriptor[] PROGMEM = {
-
-  //  Keyboard
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)  // 47
-    0x09, 0x06,                    // USAGE (Keyboard)
-    0xa1, 0x01,                    // COLLECTION (Application)
-    0x85, 0x02,                    //   REPORT_ID (2)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-   
-  0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)
-    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-    0x75, 0x01,                    //   REPORT_SIZE (1)
-    
-  0x95, 0x08,                    //   REPORT_COUNT (8)
-    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-    0x95, 0x01,                    //   REPORT_COUNT (1)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs)
-    
-  0x95, 0x06,                    //   REPORT_COUNT (6)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
-    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)
-    
-  0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))
-    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
-    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
-    0xc0,                          // END_COLLECTION
+	//  Keyboard
+	0x05, 0x01,          // USAGE_PAGE (Generic Desktop)
+	0x09, 0x06,          // USAGE (Keyboard)
+	0xa1, 0x01,          // COLLECTION (Application)
+	0x85, 0x02,          //   REPORT_ID (2)
+	0x05, 0x07,          //   USAGE_PAGE (Keyboard)
+	// Modifier keys
+	0x19, 0xe0,          //   USAGE_MINIMUM (Keyboard Left Control)
+	0x29, 0xe7,          //   USAGE_MAXIMUM (Keyboard Right GUI)
+	0x15, 0x00,          //   LOGICAL_MINIMUM (0)
+	0x25, 0x01,          //   LOGICAL_MAXIMUM (1)
+	0x75, 0x01,          //   REPORT_SIZE (1)
+	0x95, 0x08,          //   REPORT_COUNT (8)
+	0x81, 0x02,          //   INPUT (Data,Var,Abs)
+	// Reserved byte
+	0x75, 0x08,          //   REPORT_SIZE (8)
+	0x95, 0x01,          //   REPORT_COUNT (1)
+	0x81, 0x03,          //   INPUT (Cnst,Var,Abs)
+	// Pressed keys
+	0x19, 0x00,          //   USAGE_MINIMUM (Reserved (no event indicated))
+	0x29, 0x65,          //   USAGE_MAXIMUM (Keyboard Application)
+	0x15, 0x00,          //   LOGICAL_MINIMUM (0)
+	0x25, 0x65,          //   LOGICAL_MAXIMUM (101)
+	0x75, 0x08,          //   REPORT_SIZE (8)
+	0x95, 0x06,          //   REPORT_COUNT (6)
+	0x81, 0x00,          //   INPUT (Data,Ary,Abs)
+	// End
+	0xc0,                // END_COLLECTION
 };
 
 Keyboard_::Keyboard_(void) 
