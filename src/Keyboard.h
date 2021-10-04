b86839cc5a7e628d81795e3e928960f97e208520
TQMatvey <77576395+TQMatvey@users.noreply.github.com>
2021-10-04 12:26:52 +0700

    HI my first PR
    commented all keyboard events... i needed it and decided to open my first ever pull request)) idk if i did it right xD

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -34,54 +34,54 @@
 //================================================================================
 //  Keyboard
 
-#define KEY_LEFT_CTRL     0x80
-#define KEY_LEFT_SHIFT    0x81
-#define KEY_LEFT_ALT      0x82
-#define KEY_LEFT_GUI      0x83
-#define KEY_RIGHT_CTRL    0x84
-#define KEY_RIGHT_SHIFT   0x85
-#define KEY_RIGHT_ALT     0x86
-#define KEY_RIGHT_GUI     0x87
+#define KEY_LEFT_CTRL     0x80		    // 128
+#define KEY_LEFT_SHIFT    0x81		    // 129
+#define KEY_LEFT_ALT      0x82		    // 130
+#define KEY_LEFT_GUI      0x83		    // 131
+#define KEY_RIGHT_CTRL    0x84		    // 132
+#define KEY_RIGHT_SHIFT   0x85		    // 133
+#define KEY_RIGHT_ALT     0x86		    // 134
+#define KEY_RIGHT_GUI     0x87		    // 135
 
-#define KEY_UP_ARROW      0xDA
-#define KEY_DOWN_ARROW    0xD9
-#define KEY_LEFT_ARROW    0xD8
-#define KEY_RIGHT_ARROW   0xD7
-#define KEY_BACKSPACE     0xB2
-#define KEY_TAB           0xB3
-#define KEY_RETURN        0xB0
-#define KEY_ESC           0xB1
-#define KEY_INSERT        0xD1
-#define KEY_DELETE        0xD4
-#define KEY_PAGE_UP       0xD3
-#define KEY_PAGE_DOWN     0xD6
-#define KEY_HOME          0xD2
-#define KEY_END           0xD5
-#define KEY_CAPS_LOCK     0xC1
-#define KEY_F1            0xC2
-#define KEY_F2            0xC3
-#define KEY_F3            0xC4
-#define KEY_F4            0xC5
-#define KEY_F5            0xC6
-#define KEY_F6            0xC7
-#define KEY_F7            0xC8
-#define KEY_F8            0xC9
-#define KEY_F9            0xCA
-#define KEY_F10           0xCB
-#define KEY_F11           0xCC
-#define KEY_F12           0xCD
-#define KEY_F13           0xF0
-#define KEY_F14           0xF1
-#define KEY_F15           0xF2
-#define KEY_F16           0xF3
-#define KEY_F17           0xF4
-#define KEY_F18           0xF5
-#define KEY_F19           0xF6
-#define KEY_F20           0xF7
-#define KEY_F21           0xF8
-#define KEY_F22           0xF9
-#define KEY_F23           0xFA
-#define KEY_F24           0xFB
+#define KEY_UP_ARROW      0xDA		    // 218
+#define KEY_DOWN_ARROW    0xD9		    // 217
+#define KEY_LEFT_ARROW    0xD8		    // 216
+#define KEY_RIGHT_ARROW   0xD7		    // 215
+#define KEY_BACKSPACE     0xB2		    // 178
+#define KEY_TAB           0xB3		    // 179
+#define KEY_RETURN        0xB0		    // 176
+#define KEY_ESC           0xB1		    // 177
+#define KEY_INSERT        0xD1		    // 209
+#define KEY_DELETE        0xD4		    // 212
+#define KEY_PAGE_UP       0xD3		    // 211
+#define KEY_PAGE_DOWN     0xD6		    // 214
+#define KEY_HOME          0xD2		    // 210
+#define KEY_END           0xD5	    	// 213
+#define KEY_CAPS_LOCK     0xC1		    // 193
+#define KEY_F1            0xC2		    // 194
+#define KEY_F2            0xC3		    // 195
+#define KEY_F3            0xC4		    // 196
+#define KEY_F4            0xC5		    // 197
+#define KEY_F5            0xC6	    	// 198
+#define KEY_F6            0xC7		    // 199
+#define KEY_F7            0xC8		    // 200
+#define KEY_F8            0xC9		    // 201
+#define KEY_F9            0xCA		    // 202
+#define KEY_F10           0xCB		    // 203
+#define KEY_F11           0xCC		    // 204
+#define KEY_F12           0xCD		    // 205
+#define KEY_F13           0xF0		    // 240
+#define KEY_F14           0xF1		    // 241
+#define KEY_F15           0xF2		    // 242
+#define KEY_F16           0xF3		    // 243
+#define KEY_F17           0xF4		    // 244
+#define KEY_F18           0xF5		    // 245
+#define KEY_F19           0xF6		    // 246
+#define KEY_F20           0xF7	    	// 247
+#define KEY_F21           0xF8		    // 248
+#define KEY_F22           0xF9		    // 249
+#define KEY_F23           0xFA		    // 250
+#define KEY_F24           0xFB	    	// 251
 
 
 // Low level key report: up to 6 keys and shift, ctrl etc at once
