b0f20ff53f16da7abf935bd84ff5ea76629c0e3b
Pete <41942933+PJ789@users.noreply.github.com>
2022-01-08 22:01:13 +0100

    Update Keyboard.cpp

--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -4,6 +4,7 @@
   Copyright (c) 2015, Arduino LLC
 
   Original code (pre-library): Copyright (c) 2011, Peter Barrett
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
