939d576e5d824bfe7c52df51448e474d96fa8784
Samuel Teixeira <samuelstj@gmail.com>
2024-11-22 20:22:47 -0300

    Add Brazilian Portuguese keyboard layout

--- src/Keyboard.h
+++ src/Keyboard.h
@@ -116,6 +116,7 @@ extern const uint8_t KeyboardLayout_en_US[];
 extern const uint8_t KeyboardLayout_es_ES[];
 extern const uint8_t KeyboardLayout_fr_FR[];
 extern const uint8_t KeyboardLayout_it_IT[];
+extern const uint8_t KeyboardLayout_pt_BR[];
 extern const uint8_t KeyboardLayout_pt_PT[];
 extern const uint8_t KeyboardLayout_sv_SE[];
 extern const uint8_t KeyboardLayout_da_DK[];
